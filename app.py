3dcb5a2ff7bc37cee7e2770a74f67e3218e40d34
dbwc <d.benseman@wc.school.nz>
2022-03-01 10:39:07 +1300

    Database table created and connected

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "smile.db"
+
+def create_connection(db_file):
+    """
+    Create a connection with the database
+    parameter: name of the database file
+    returns: a connection to the file
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route('/')
@@ -10,7 +26,15 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
-    return render_template('menu.html')
+    con = create_connection(DATABASE)
+
+    query = "SELECT name, description, volume, price, image FROM product"
+    cur = con.cursor()     # Creates a cursor to write the query
+    cur.execute(query)     # Runs the query
+    product_list = cur.fetchall()
+    con.close()
+
+    return render_template('menu.html', products=product_list)
 
 
 @app.route('/contact')
