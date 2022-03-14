6ac740d1c17d8f51ee45d7391187106a41b37dec
dbwc <d.benseman@wc.school.nz>
2022-03-15 10:05:23 +1300

    Login complete and encrypted

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from sqlite3 import Error
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = "lgfjh ahg lkg hg hldhg kjfxzh hds g;jh"
 DATABASE = "C:/Users/d.benseman/OneDrive - Wellington College/13DTS/Python2022/13-3/Smile/smile.db"
 
 
@@ -20,9 +23,21 @@ def create_connection(db_file):
     return None
 
 
+def is_logged_in():
+    """
+    A function to return whether the user is logged in or not
+    """
+    if session.get("email") is None:
+        print("Not logged in")
+        return False
+    else:
+        print("Logged in")
+        return True
+
+
 @app.route('/')
 def render_homepage():
-    return render_template('home.html')
+    return render_template('home.html', logged_in=is_logged_in())
 
 
 @app.route('/menu')
@@ -36,12 +51,12 @@ def render_menu_page():
     print(product_list)
     con.close()
 
-    return render_template('menu.html', products=product_list)
+    return render_template('menu.html', products=product_list, logged_in=is_logged_in())
 
 
 @app.route('/contact')
 def render_contact_page():
-    return render_template('contact.html')
+    return render_template('contact.html', logged_in=is_logged_in())
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -51,21 +66,39 @@ def render_login_page():
         email = request.form.get('email').lower().strip()
         password = request.form.get('password')
 
+
         con = create_connection(DATABASE)
-        query = "SELECT id, fname FROM customer WHERE email=? AND password=?"
+        query = "SELECT id, fname, password FROM customer WHERE email=?"
         cur = con.cursor()
-        cur.execute(query, (email, password))
+        cur.execute(query, (email, ))
         user_data = cur.fetchall()
         con.close()
 
-        try:
+        if user_data:
             user_id = user_data[0][0]
             first_name = user_data[0][1]
-        except indexError:
-            return redirect("login?error=Email+or+password+is+incorrect")
-        print(user_id, first_name)
+            db_password = user_data[0][2]
+        else:
+            return redirect("/login?error=Email+or+password+is+incorrect")
+            # Set up a session for this login
 
-    return render_template('login.html')
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect("/login?error=Email+or+password+is+incorrect")
+
+        session['email'] = email
+        session['user_id'] = user_id
+        session['fname'] = first_name
+        session['cart'] = []
+        return redirect('/')
+    return render_template('login.html', logged_in=is_logged_in())
+
+
+@app.route('/logout')
+def render_logout_page():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=See+you+next+time')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -85,12 +118,13 @@ def render_signup_page():
         if len(password) < 8:
             return redirect('/signup?error=Password+must+be+at+least+8+characters')
 
+        hashed_password = bcrypt.generate_password_hash(password)
         con = create_connection(DATABASE)
 
         query = "INSERT INTO customer (fname, lname, email, password) VALUES (?, ?, ?, ?)"
 
         cur = con.cursor()
-        cur.execute(query, (fname, lname, email, password))
+        cur.execute(query, (fname, lname, email, hashed_password))
         con.commit()
         con.close()
         return redirect('/login')
@@ -98,7 +132,7 @@ def render_signup_page():
     error = request.args.get('error')
     if error == None:
         error = ""
-    return render_template('signup.html', error=error)
+    return render_template('signup.html', error=error, logged_in=is_logged_in())
 
 
 app.run(host='127.0.0.1', debug=True)
